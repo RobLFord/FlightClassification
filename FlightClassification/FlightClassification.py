f94dd0f325f3b7d6ed2d29ef6af7bd864e0827b0
seniark <11bkrain@gmail.com>
2017-06-11 12:26:49 -0400

    Extract day of week from epoch time, add boolean verification field int'l/not for use in training/verification

--- FlightClassification/FlightClassification.py
+++ FlightClassification/FlightClassification.py
@@ -2,6 +2,7 @@
 import numpy as np
 import pandas as pd
 import ijson
+import time
 
 def create_df():
 	good_attributes = [
@@ -17,7 +18,6 @@ def create_df():
 		'TSecs',
 		'Type',
 		'To',
-		'From',
 		'Cou',
 		'Species',
 		'Mil',
@@ -28,7 +28,7 @@ def create_df():
 
 	useFile = True
 	if useFile:
-		filename = '2017-05-29-0000Z.json'
+		filename = '2016-06-20-0000Z.json'
 		f= open(filename, 'r', encoding="utf8")
 		objects = ijson.items(f, 'acList.item')
 	#else:
@@ -56,130 +56,123 @@ def create_df():
 
 	
 def preprocessing(df):
-	#check to make sure there are not duplicate Ids
 
-	"""Ids_dup = df.duplicated('Id')
-	for IDs in Ids_dup:
-		if IDs:
-			print('There are some duplicates in Id')
-			error = True
-		else:
-			error = False
-	"""
-	error = False
-	if not error:
-		#See if there are any missing values
-		# print(pd.isnull(df[:30]))
+	# Remove flights that have landed
+	GROUNDED = df[df['Gnd'] == True].index.tolist()
+	df = df.drop(df.index[GROUNDED],)
 
-		# Remove flights that have landed
-		GROUNDED = df[df['Gnd'] == True].index.tolist()
-		df = df.drop(df.index[GROUNDED],)
+	#Remove flights without GPS information
+	no_gps = df[pd.isnull(df['Lat'])].index.tolist()
+	df = df.drop(no_gps)
 
-		#Remove flights without GPS information
-		no_gps = df[pd.isnull(df['Lat'])].index.tolist()
-		df = df.drop(no_gps)
+	#Remove flights without Destination Information
+	#WARNING: only for training set pre-process
+	#Not sure if this is the final strategy or need to utilize 3rd party destination source
+	no_dest = df[pd.isnull(df['To'])].index.tolist()
+	df = df.drop(no_dest)
 
-		#Remove flights without Destination Information
-		#WARNING: only for training set pre-process
-		#Not sure if this is the final strategy or need to utilize 3rd party destination source
-		no_dest = df[pd.isnull(df['To'])].index.tolist()
-		df = df.drop(no_dest)
-		
-		# Changing target classifier to have International and Domestic Fields
-		US = df.Cou == 'United States'
-		NONUS = df.Cou != 'United States'
-		
-		df.loc[US,'Cou'] = 'Domestic'
-		df.loc[NONUS,'Cou'] = 'International'
-		
-		# Fixing values with negative values
-		LESSTHAN_0 = df.Alt < 0
-		df.loc[LESSTHAN_0,'Alt'] = None
-		
-		#Replace the some of missing values with average of column
-		"""
-		df.Spd = df.Spd.fillna(df.Spd.mean().round())
-		df.Alt = df.Alt.fillna(df.Alt.mean())
-		df.Alt = df.Alt.round()
-		df.TSecs = df.TSecs.fillna(df.TSecs.mean())
-		df.TSecs = df.TSecs.round()
-		"""
+	# Changing target classifier to have International and Domestic Fields
+	US = df.Cou == 'United States'
+	NONUS = df.Cou != 'United States'
 
-		#Limit results to near JFK Int'l Airport
-		#JFK Latitude: 40.644623
-		#JFK Longitude: -73.784180
-		#Let's try a 50 mi radius around the airport, using www.gpsvisualizer.com
-		#dlat = .726358
-		#dlon = .972934
-		#Max Latitude: 41.370981
-		#Min Latitude: 39.918265
-		#Max Longitude: -72.811246
-		#Min Longitude: -74.757114
+	df.loc[US,'Cou'] = 'Domestic'
+	df.loc[NONUS,'Cou'] = 'International'
 
-		#Latitude Constraint
-		outside_latitude_bounds = df[df['Lat'] < 39.9182].index.tolist()
-		outside_latitude_bounds += df[df['Lat'] > 41.3710].index.tolist()
-		df = df.drop(outside_latitude_bounds)
+	# Fixing values with negative values
+	LESSTHAN_0 = df.Alt < 0
+	df.loc[LESSTHAN_0,'Alt'] = None
 
-		#Longitude Constraint
-		outside_longitude_bounds = df[df['Long'] < -74.7571].index.tolist()
-		outside_longitude_bounds += df[df['Long'] > -72.8112].index.tolist()
-		df = df.drop(outside_longitude_bounds)
-				
-		bad_attributes = [
-			'Id',
-			'Gnd',
-			'Species',
-			'Mil',
-		]
-			
-		for attribute in bad_attributes:
-			del df[attribute]
-			
-	return(error,df)
+	# Extract Fields from Epoch Time
+	df['DateTime'] = pd.to_datetime(df['PosTime'], unit='ms')
+	df['Weekday'] = df['DateTime'].dt.dayofweek
+
+	#Create boolean field for whether Int'l Flight - training/verification use
+	df['Intl'] = df['To'].astype(str).str[0] != 'K'
+
+
+
+
+	###############################################
+	#Limit results to near JFK Int'l Airport
+	#JFK Latitude: 40.644623
+	#JFK Longitude: -73.784180
+	#Let's try a 50 mi radius around the airport, using www.gpsvisualizer.com
+	#dlat = .726358
+	#dlon = .972934
+	#Max Latitude: 41.370981
+	#Min Latitude: 39.918265
+	#Max Longitude: -72.811246
+	#Min Longitude: -74.757114
+	##############################################
+
+	#Latitude Constraint
+	outside_latitude_bounds = df[df['Lat'] < 39.9182].index.tolist()
+	outside_latitude_bounds += df[df['Lat'] > 41.3710].index.tolist()
+	df = df.drop(outside_latitude_bounds)
+
+	#Longitude Constraint
+	outside_longitude_bounds = df[df['Long'] < -74.7571].index.tolist()
+	outside_longitude_bounds += df[df['Long'] > -72.8112].index.tolist()
+	df = df.drop(outside_longitude_bounds)
+
+	bad_attributes = [
+		'Id',
+		'Gnd',
+		'Species',
+		'Mil',
+		'PosTime',
+		'DateTime',
+		'Lat',
+		'Long'
+	]
+
+	for attribute in bad_attributes:
+		del df[attribute]
+
+	return df
+
+def main():
+	df = create_df()
+	df = preprocessing(df)
+
+	print(df.to_string()) #If receive encoding error enter for windows $ chcp 65001
+	json_out = df.to_json()
+	csv_out = df.to_csv()
+	json_out_file = open("test_out.json", "w")
+	json_out_file.write(json_out)
+	json_out_file.close()
+	csv_out_file = open("test_out.csv", "w")
+	csv_out_file.write(csv_out)
+	csv_out_file.close()
+	print("Resultant Data Set Contains " + str(df.shape[0]) + " Records...")
+	print("Done.")
+
+	# ------------------- This Section had not been tested yet ------------------------#
+	# from sklearn.cross_validation import train_test_split
+	# from sklearn.tree import DecisionTreeClassifier
+	# from sklearn.metrics import accuracy_score
+	# from sklearn import tree
+	# X_train, X_test, y_train, y_test = train_test_split( X, Y, test_size = 0.3, random_state = 100)
+
+	# clf_gini = DecisionTreeClassifier(criterion = "gini", random_state = 100,
+									# max_depth=3, min_samples_leaf=5)
+
+	# clf_gini.fit(X_train, y_train)
+
+	# clf_entropy = DecisionTreeClassifier(criterion = "entropy", random_state = 100,
+									# max_depth=3, min_samples_leaf=5)
+
+	# clf_entropy.fit(X_train, y_train)
+
+	# y_pred = clf_gini.predict(X_test)
+
+	# y_pred_en = clf_entropy.predict(X_test)
+
+	# print("Accuracy of Gini ", accuracy_score(y_test,y_pred)*100)
+
+	# print("Accuracy of Entropy ", accuracy_score(y_test,y_pred_en)*100)
+
+	#----------------------------------------------------------------------------------#
 
 if __name__ == '__main__':
-	df = create_df()
-	error, df = preprocessing(df)
-	
-	if not error:
-		print(df.to_string()) #If receive encoding error enter for windows $ chcp 65001
-		json_out = df.to_json()
-		csv_out = df.to_csv()
-		json_out_file = open("test_out.json", "w")
-		json_out_file.write(json_out)
-		json_out_file.close()
-		csv_out_file = open("test_out.csv", "w")
-		csv_out_file.write(csv_out)
-		csv_out_file.close()
-		print("Resultant Data Set Contains " + str(df.shape[0]) + " Records...")
-		print("Done.")
-		
-		# ------------------- This Section had not been tested yet ------------------------#
-		# from sklearn.cross_validation import train_test_split
-		# from sklearn.tree import DecisionTreeClassifier
-		# from sklearn.metrics import accuracy_score
-		# from sklearn import tree
-		# X_train, X_test, y_train, y_test = train_test_split( X, Y, test_size = 0.3, random_state = 100)
-		
-		# clf_gini = DecisionTreeClassifier(criterion = "gini", random_state = 100,
-										# max_depth=3, min_samples_leaf=5)
-										
-		# clf_gini.fit(X_train, y_train)
-		
-		# clf_entropy = DecisionTreeClassifier(criterion = "entropy", random_state = 100,
-										# max_depth=3, min_samples_leaf=5)
-										
-		# clf_entropy.fit(X_train, y_train)
-		
-		# y_pred = clf_gini.predict(X_test)
-		
-		# y_pred_en = clf_entropy.predict(X_test)
-		
-		# print("Accuracy of Gini ", accuracy_score(y_test,y_pred)*100)
-		
-		# print("Accuracy of Entropy ", accuracy_score(y_test,y_pred_en)*100)
-		
-		#----------------------------------------------------------------------------------#
-
-
+	main()
